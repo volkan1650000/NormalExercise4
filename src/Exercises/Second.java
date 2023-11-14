8f96ad37b993e718df21ebc9b392e22a4b562ea6
volkan1650000 <133212647+volkan1650000@users.noreply.github.com>
2023-11-14 23:05:22 +0100

    Update Second.java

--- src/Exercises/Second.java
+++ src/Exercises/Second.java
@@ -1,5 +1,7 @@
 package Exercises;
 
+//A method to return after taking a string as input and reverses the order of words in the string while preserving the spaces between them.
+
 public class Second {
     public static void main(String[] args) {
         String a = "a good   example     of this code";
